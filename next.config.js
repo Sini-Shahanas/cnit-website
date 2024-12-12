ba144679e127bc8dd95d0d04c181476ec3980398
Sini-Shahanas <sini.shahanas@cnitsolution.com>
2024-12-12 20:35:43 +0400

    Some changes

--- next.config.js
+++ next.config.js
@@ -11,8 +11,8 @@ const nextConfig = {
   },
   images: {
     // domains: ['localhost', 'localhost:1337'],
-    domains: ['localhost'],
-    // domains: ['admin.cnit-solutions.com'],
+    // domains: ['localhost'],
+    domains: ['admin.cnit-solutions.com'],
   },
   // distDir: 'dist_build',
 }
